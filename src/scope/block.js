fe8e5544dceed769b1f83cd2d52baa1e4f2ff8b7
jl-molina <joseluisml1218@gmail.com>
2023-01-22 15:10:53 -0700

    Block Scope

---
+++ src/scope/block.js
@@ -0,0 +1,12 @@
+function fruit() {
+    if (true) {
+        var fruit1 = 'Apple';
+        let fruit2 = 'Kiwi';
+        let fruit3 = 'Banana';
+        console.log(fruit2);
+        console.log(fruit3);
+    }
+    console.log(fruit1);
+}
+
+fruit();
