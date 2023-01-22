b6e7bb4354a64f96c31638a1b1bee7bafe1d8521
jl-molina <joseluisml1218@gmail.com>
2023-01-21 20:54:23 -0700

    Function Scope

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting() {
+    let userName = 'Jose';
+    console.log(userName);
+
+    if (userName === 'Jose') {
+        console.log(`Hello, ${userName}`);
+    }
+}
+
+greeting();
+
+console.log(userName);
