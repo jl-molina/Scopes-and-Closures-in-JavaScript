bc9dca55417004383d1519a0f0f3c73c3fe06e3f
jl-molina <joseluisml1218@gmail.com>
2023-01-22 15:25:45 -0700

    Reassign and Redeclare

---
+++ src/scope/reassign-redeclare.js
@@ -0,0 +1,30 @@
+var firstName; // undefined
+firstName = 'Jose';
+console.log(firstName);
+
+//Reassign
+var lastName = 'Molina';
+lastName = 'Leal';
+console.log(lastName);
+
+//Redeclare
+var secondName = 'Luis';
+var secondName = 'Pedro';
+console.log(secondName);
+
+//Let
+let fruit = 'Apple';
+// let fruit = 'Kiwi';
+fruit = 'Kiwi';
+console.log(fruit);
+
+//Const
+const animal = 'dog';
+// animal = 'cat';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('car');
+console.log(vehicles);
+vehicles.pop();
+console.log(vehicles);
