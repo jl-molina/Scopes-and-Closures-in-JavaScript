ed6e3dd1c0c5905f10127b743321aff9dda94fa7
jl-molina <joseluisml1218@gmail.com>
2023-02-09 16:21:50 -0700

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+console.log(nameOfDog);
+var nameOfDog = "Elmo";
+console.log(nameOfDog);
+
+getNameOfDog();
+
+function getNameOfDog() {
+    console.log(`The best dog is ${elmo}`);
+}
+
+var elmo = 'Elmito';
